6a74e62ee2d5effbdefe6f40c5352145f972e488
David Johnson <djohn270@uncc.edu>
2019-03-23 01:21:30 +0000

    Add comments routes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_23_000453) do
+ActiveRecord::Schema.define(version: 2019_03_23_010734) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2019_03_23_000453) do
   create_table "comments", force: :cascade do |t|
     t.string "commenter"
     t.text "body"
+    t.text "date"
     t.integer "article_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
